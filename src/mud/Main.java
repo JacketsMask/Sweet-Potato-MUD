6bb9b47679878f68a5d4963294435ae1c86c9894
Japhez <JaphezBeMeYe@hotmail.com>
2013-07-08 14:21:21 -0400

    Added functionality to allow for conditional server hosting based off of client preferences.

--- src/mud/Main.java
+++ src/mud/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 import mud.network.client.ClientFrame;
 import mud.network.client.GameClient;
+import mud.network.client.GameClient.ConnectionChoice;
 import mud.network.server.GameServer;
 
 /**
@@ -12,26 +13,34 @@ import mud.network.server.GameServer;
  */
 public class Main {
 
-    final static int PORT = 1337;
-
     /**
      * Starts the server on a new thread and listens for client connections.
      *
      * @throws IOException
      */
-    public static void startServer() throws IOException {
-        GameServer gameServer = new GameServer(PORT);
+    public static void startServer(boolean localOnly) throws IOException {
+        GameServer gameServer = new GameServer(GameServer.DEFAULT_PORT, localOnly);
         new Thread(gameServer).start();
     }
 
     public static void connectClient() throws UnknownHostException, IOException {
         ClientFrame clientFrame = new ClientFrame();
-        GameClient gameClient = new GameClient("localhost", PORT, clientFrame.getjTextArea1(), clientFrame.getjTextField1());
+        GameClient gameClient = new GameClient(clientFrame.getjTextArea1(), clientFrame.getjTextField1());
         clientFrame.setVisible(true);
+        //Block until the connection choice is determined
+        ConnectionChoice connectionChoice = gameClient.getConnectionChoice();
+        //Check for local only play
+        if (connectionChoice.equals(ConnectionChoice.LOCAL_SOLO)) {
+            startServer(true);
+        }
+        //Check for local hosting
+        if (connectionChoice.equals(ConnectionChoice.LOCAL_CO_OP)) {
+            startServer(true);
+        }
+        //Otherwise the connection is remote, no need for server
     }
 
     public static void main(String[] args) throws IOException {
-        startServer();
         connectClient();
     }
 }
