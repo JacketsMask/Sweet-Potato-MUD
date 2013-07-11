69a6ed8687fae54bfe8b4eedb5cf0cc59d3a90bf
Japhez <JaphezBeMeYe@hotmail.com>
2013-07-10 21:24:23 -0400

    Consolidated unnecessary methods.  Created methods for room modification.

--- src/mud/geography/Room.java
+++ src/mud/geography/Room.java
@@ -2,6 +2,7 @@ package mud.geography;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import mud.AreaManager;
 import mud.Player;
 
 /**
@@ -11,6 +12,7 @@ import mud.Player;
  */
 public class Room implements Serializable {
 
+    private Area area;
     private int roomID;
     private String name;
     private String description;
@@ -25,11 +27,65 @@ public class Room implements Serializable {
     //private ArrayList<NPC> npcs; //TODO: Milestone #2
     private ArrayList<Player> players;
 
-    public Room(int roomID) {
-        this.roomID = roomID;
+    public Room(Area area, AreaManager areaManger) {
+        //Set the area
+        this.area = area;
+        //Get a unique room ID
+        this.roomID = areaManger.getUniqueRoomID();
+        //Set a default name
         name = "Unnamed Room";
+        //Set a default description
         description = "You're in a nondescript room.";
+        //Initialize player list
         players = new ArrayList<>();
+        //Add the room to the area
+        area.addRoom(this);
+        //Add the room to the global list
+        areaManger.addRoom(this);
+    }
+
+    /**
+     * Links this room through the passed direction to the passed room. They are
+     * linked both ways.
+     *
+     * @param direction
+     * @param room
+     * @return true if successful, false if not
+     */
+    public boolean linkToRoom(Direction direction, Room room) {
+        //If either room is already linked with the necessary direction, return false
+        if (getRoomInDirection(direction) != null || room.getRoomInDirection(Direction.getOppositeDirection(direction)) != null) {
+            return false;
+        } else {
+            //Link this room to that
+            setRoomInDirection(direction, room);
+            //Link that room to this
+            room.setRoomInDirection(Direction.getOppositeDirection(direction), this);
+            return true;
+        }
+    }
+
+    /**
+     * Unlinks the passed exists from this room and the connected rooms.
+     *
+     * @param exits the exits to unlink
+     * @param room the room whose exits to unlink
+     */
+    public void unlinkExits(Direction[] exits) {
+        //For each direction
+        for (Direction d : exits) {
+            //If the direction isn't null
+            if (getRoomInDirection(d) != null) {
+                //Unlink the linked room's connection
+                getRoomInDirection(d).setRoomInDirection(Direction.getOppositeDirection(d), null);
+                //Unlink this direction
+                setRoomInDirection(d, null);
+            }
+        }
+    }
+
+    public Area getArea() {
+        return area;
     }
 
     public synchronized void addPlayer(Player player) {
@@ -40,15 +96,11 @@ public class Room implements Serializable {
         return players;
     }
 
-    public synchronized int getRoomID() {
+    public int getRoomID() {
         return roomID;
     }
 
-    public synchronized void setRoomID(int roomID) {
-        this.roomID = roomID;
-    }
-
-    public synchronized String getName() {
+    public String getName() {
         return name;
     }
 
@@ -56,40 +108,12 @@ public class Room implements Serializable {
         this.name = name;
     }
 
-    public synchronized String getDescription() {
+    public String getDescription() {
         return description;
     }
 
     public synchronized void setDescription(String description) {
         this.description = description;
-    }
-
-    public synchronized void setNorth(Room north) {
-        this.north = north;
-    }
-
-    public synchronized void setEast(Room east) {
-        this.east = east;
-    }
-
-    public synchronized void setSouth(Room south) {
-        this.south = south;
-    }
-
-    public synchronized Room getWest() {
-        return west;
-    }
-
-    public synchronized void setWest(Room west) {
-        this.west = west;
-    }
-
-    public synchronized void setUp(Room up) {
-        this.up = up;
-    }
-
-    public synchronized void setDown(Room down) {
-        this.down = down;
     }
 
     public synchronized void removePlayer(Player player) {
@@ -123,14 +147,13 @@ public class Room implements Serializable {
             return down;
         }
         return null;
-
     }
 
     /**
      * @return the exists to this room in a formatted string, or null if there
      * are no exits
      */
-    public synchronized String getExits() {
+    public synchronized String getFormattedExits() {
         if (north == null && east == null && south == null && west == null && up == null && down == null) {
             return "You're trapped!  You can't see any exits!";
         }
@@ -156,5 +179,36 @@ public class Room implements Serializable {
         //Add last pipe
         exits += "|";
         return exits;
+    }
+
+    /**
+     * Sets the exit in the given direction to the given room.
+     *
+     * @param direction
+     * @param newRoom
+     */
+    public synchronized void setRoomInDirection(Direction direction, Room newRoom) {
+        if (direction.equals(Direction.NORTH)) {
+            north = newRoom;
+        }
+        if (direction.equals(Direction.EAST)) {
+            east = newRoom;
+        }
+        if (direction.equals(Direction.SOUTH)) {
+            south = newRoom;
+        }
+        if (direction.equals(Direction.WEST)) {
+            west = newRoom;
+        }
+        if (direction.equals(Direction.UP)) {
+            up = newRoom;
+        }
+        if (direction.equals(Direction.DOWN)) {
+            down = newRoom;
+        }
+    }
+    
+    public void sendMessageToRoom(Player sender) {
+        //TODO: do this at some point
     }
 }
