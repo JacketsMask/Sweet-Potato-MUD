c815797b62eae846373a359f08f6017b5cd5ffd5
Japhez <JaphezBeMeYe@hotmail.com>
2013-07-10 21:29:45 -0400

    Added master room list for ease of lookup, and improved room management.

--- src/mud/AreaManager.java
+++ src/mud/AreaManager.java
@@ -1,10 +1,12 @@
 package mud;
 
 import file.FileManipulator;
+import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import mud.geography.Area;
+import mud.geography.Direction;
 import mud.geography.Room;
 import mud.network.server.log.ConsoleLog;
 
@@ -21,10 +23,12 @@ public final class AreaManager implements Serializable {
     private ArrayList<Integer> freeAreaIDs;
     //A master list of all areas in the MUD
     private HashMap<Integer, Area> masterAreaList;
+    //A master list of all rooms in the MUD
+    private HashMap<Integer, Room> masterRoomList;
     private int roomIDCount;
     private ArrayList<Integer> freeRoomIDs;
     private Room respawnRoom;
-    private final String PATH = "/areas/";
+    private final String PATH = "areas//";
     private final String EXTENSION = ".area";
 
     public AreaManager() {
@@ -33,8 +37,24 @@ public final class AreaManager implements Serializable {
         freeRoomIDs = new ArrayList<>();
         freeAreaIDs = new ArrayList<>();
         masterAreaList = new HashMap<>();
+        masterRoomList = new HashMap<>();
         System.out.println(ConsoleLog.log() + "Creating starter area.");
         createStarterArea();
+    }
+
+    public void removeOrphanRooms() {
+        //TODO: Scan all areas for rooms without exits, delete any disconnected rooms
+    }
+
+    /**
+     * Returns the room with the given ID, if the room exists. If it doesn't
+     * exist, returns null.
+     *
+     * @param roomID
+     * @return the room if it exists, otherwise null
+     */
+    public Room getRoom(int roomID) {
+        return masterRoomList.get(roomID);
     }
 
     /**
@@ -44,7 +64,8 @@ public final class AreaManager implements Serializable {
      * @param area the area to be saved
      */
     public void saveArea(Area area) {
-        FileManipulator.writeObject(area, GameMaster.MAIN_DATA_PATH + PATH, area.getName() + EXTENSION);
+        FileManipulator.writeObject(area, (GameMaster.MAIN_DATA_PATH + PATH), area.getName() + EXTENSION);
+        System.out.println(ConsoleLog.log() + area.getName() + " saved locally.");
     }
 
     /**
@@ -55,41 +76,37 @@ public final class AreaManager implements Serializable {
         //Create a new area with a new area ID
         Area testArea = new Area("Test Area", getUniqueAreaID());
         //Create 5 rooms
-        Room center = new Room(getUniqueRoomID());
-        Room north = new Room(getUniqueRoomID());
-        Room east = new Room(getUniqueRoomID());
-        Room south = new Room(getUniqueRoomID());
-        Room west = new Room(getUniqueRoomID());
-        //Set the name and exits for the center room
+        Room center = new Room(testArea, this);
+        Room north = new Room(testArea, this);
+        Room east = new Room(testArea, this);
+        Room south = new Room(testArea, this);
+        Room west = new Room(testArea, this);
+        //Set room names and exists
         center.setName("Center room.");
-        center.setNorth(north);
-        center.setEast(east);
-        center.setSouth(south);
-        center.setWest(west);
+        north.setName("North room.");
+        east.setName("East room.");
+        south.setName("South room.");
+        west.setName("West room.");
+        center.linkToRoom(Direction.NORTH, north);
+        center.linkToRoom(Direction.EAST, east);
+        center.linkToRoom(Direction.SOUTH, south);
+        center.linkToRoom(Direction.WEST, west);
         //Set the respawn room
         setRespawnRoom(center);
-        //Set the name and exits for the northern room
-        north.setName("North room.");
-        north.setSouth(center);
-        //Set the name and exits for the eastern room
-        east.setName("East room.");
-        east.setWest(center);
-        //Set the name and exits for the southern room
-        south.setName("South room.");
-        south.setNorth(center);
-        //Set the name and exits for the western room
-        west.setName("West room.");
-        west.setEast(center);
-        //All all of the rooms to the test area
-        testArea.addRoom(center);
-        testArea.addRoom(north);
-        testArea.addRoom(east);
-        testArea.addRoom(south);
-        testArea.addRoom(west);
         //Save the area to a file
         saveArea(testArea);
         //Add the area to the master area list
         masterAreaList.put(getUniqueAreaID(), testArea);
+    }
+
+    /**
+     * Deletes the passed room after unlinking all its exits.
+     *
+     * @param room
+     */
+    public void deleteRoom(Room room) {
+        room.unlinkExits(Direction.getDirections());
+        masterRoomList.remove(room.getRoomID());
     }
 
     /**
@@ -131,7 +148,14 @@ public final class AreaManager implements Serializable {
         }
     }
 
-    public static void main(String[] args) {
-        
+    /**
+     * You don't need to call this manually. It adds the passed room to the
+     * master list, but this method is called when a room is created initially
+     * anyways. So...yeah. Don't call this, you'll look silly.
+     *
+     * @param room
+     */
+    public void addRoom(Room room) {
+        masterRoomList.put(room.getRoomID(), room);
     }
 }
