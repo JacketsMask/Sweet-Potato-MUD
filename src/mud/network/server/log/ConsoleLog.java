543e7b3f07460d1371bf5b8cdd3061f3fd46d8e5
mike <mike@creeper>
2013-07-11 20:05:15 -0400

    Fixed incorrect timestamp.  Format string for SimpleDateFormat was incorrect; MM is month number, mm is minutes.

--- src/mud/network/server/log/ConsoleLog.java
+++ src/mud/network/server/log/ConsoleLog.java
@@ -2,6 +2,7 @@ package mud.network.server.log;
 
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 /**
  * A class whose only purpose is to be used with its static log() method to
@@ -12,7 +13,7 @@ import java.util.GregorianCalendar;
 public abstract class ConsoleLog {
 
     static final GregorianCalendar calendar = new GregorianCalendar();
-    static final SimpleDateFormat format = new SimpleDateFormat("HH:MM");
+    static final SimpleDateFormat format = new SimpleDateFormat("HH:mm");
 
     public static String log() {
         return "[" + format.format(calendar.getTime()) + "] ";
