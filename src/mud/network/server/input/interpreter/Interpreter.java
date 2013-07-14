ba5281a9f013e16439c7b13dce114fd510f2ad8c
CoffeeFingers <furiousthemonkeyboy@gmail.com>
2013-07-14 13:15:00 -0400

    Changed from interface to abstract class, this allows for one declaration of the HashMap and GameMaster, and allows for common functionality to be consolidated to one declaration.

---
+++ src/mud/network/server/input/interpreter/Interpreter.java
@@ -0,0 +1,35 @@
+package mud.network.server.input.interpreter;
+
+import java.net.InetAddress;
+import java.util.HashMap;
+import mud.GameMaster;
+import mud.network.server.Connection;
+
+/**
+ * An interface to be implemented by all interpreters.
+ *
+ * @author Japhez
+ */
+public abstract class Interpreter {
+    
+    protected HashMap<InetAddress, Connection> clientMap;
+    protected GameMaster master;
+    
+    public abstract boolean interpret(Connection sender, ParsedInput input);
+    
+    /**
+     * Removes the client from the client map.
+     * @param client the client to be removed
+     */
+    public final void removeFromClientMap(Connection client) {
+        clientMap.remove(client.getClientAddress());
+    }
+    
+    /**
+     * Removes the client from the client map.
+     * @param address the address (key) of the client to be removed
+     */
+    public final void removeFromClientMap(InetAddress address) {
+        clientMap.remove(address);
+    }
+}
